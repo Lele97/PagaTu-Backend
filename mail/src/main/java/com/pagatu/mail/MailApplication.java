f22cf9b874ef2be6ccc483437bcbecbb3e34ff98
Lele97 <gabriele.grandienetti@ehotmail.com.>
2025-05-14 16:52:06 +0200

    commit

--- mail/src/main/java/com/pagatu/mail/MailApplication.java
+++ mail/src/main/java/com/pagatu/mail/MailApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MailApplication {
@@ -12,5 +11,4 @@ public class MailApplication {
     public static void main(String[] args) {
         SpringApplication.run(MailApplication.class, args);
     }
-
 }
