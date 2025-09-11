a815f078b41f774df7d44fd951eee6ec328b81c4
lele97 <gabriele.grandinetti@hotmail.com>
2025-09-11 23:51:43 +0200

    service coffee adjust code quality

--- coffee/src/main/java/com/pagatu/coffee/config/KafkaProducerConfig.java
+++ coffee/src/main/java/com/pagatu/coffee/config/KafkaProducerConfig.java
@@ -16,12 +16,19 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ *
+ */
 @Configuration
 public class KafkaProducerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    /**
+     *
+     * @return
+     */
     @Bean
     public ProducerFactory<String, ProssimoPagamentoEvent> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
@@ -31,11 +38,19 @@ public class KafkaProducerConfig {
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
+    /**
+     *
+     * @return
+     */
     @Bean
     public KafkaTemplate<String, ProssimoPagamentoEvent> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 
+    /**
+     *
+     * @return
+     */
     @Bean
     public ProducerFactory<String, InvitaionEvent> producerFactory_invitation() {
         Map<String, Object> configProps = new HashMap<>();
@@ -45,11 +60,19 @@ public class KafkaProducerConfig {
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
+    /**
+     *
+     * @return
+     */
     @Bean
     public KafkaTemplate<String, InvitaionEvent> kafkaTemplate_invitation(){
         return new KafkaTemplate<>(producerFactory_invitation());
     }
 
+    /**
+     *
+     * @return
+     */
     @Bean
     public ProducerFactory<String, SaltaPagamentoEvent> producerFactory_salta_pagamenti() {
         Map<String, Object> configProps = new HashMap<>();
@@ -59,6 +82,10 @@ public class KafkaProducerConfig {
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
+    /**
+     *
+     * @return
+     */
     @Bean
     public KafkaTemplate<String, SaltaPagamentoEvent> kafkaTemplate_salta_pagamenti() {
         return new KafkaTemplate<>(producerFactory_salta_pagamenti());
--- coffee/src/main/java/com/pagatu/coffee/jwt/JwtUtil.java
+++ coffee/src/main/java/com/pagatu/coffee/jwt/JwtUtil.java
@@ -10,7 +10,7 @@ import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 
 @Component
-public class jwtUtil {
+public class JwtUtil {
 
     @Value("${jwt.secret}")
     private String secret;
