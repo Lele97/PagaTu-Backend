7e06e9bc55a509af44bbb77ed669dd88fbdfeb40
Lele97 <gabriele.grandinetti@hotmail.com>
2025-09-17 20:25:08 +0200

    service mail adjust code quality

--- mail/src/main/java/com/pagatu/mail/MailApplication.java
+++ mail/src/main/java/com/pagatu/mail/MailApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ *
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MailApplication {
 
+    /**
+     * Main method to start the Spring Boot application.
+     *
+     * @param args command line arguments passed to the application
+     */
     public static void main(String[] args) {
         SpringApplication.run(MailApplication.class, args);
     }
