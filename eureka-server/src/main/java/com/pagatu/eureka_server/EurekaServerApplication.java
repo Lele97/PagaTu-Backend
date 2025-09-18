b1a55cc74cd2a57503ba9067c42a9c2d934cc30c
Lele97 <gabriele.grandinetti@hotmail.com>
2025-09-18 17:49:42 +0200

    service eureka-server adjust code quality

--- eureka-server/src/main/java/com/pagatu/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/pagatu/eureka_server/EurekaServerApplication.java
@@ -12,6 +12,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApplication {
 
+	/**
+	 * Main method to start the Spring Boot application.
+	 *
+	 * @param args command line arguments passed to the application
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
