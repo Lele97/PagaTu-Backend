8555dd459846a21e94562b2cf67af5557c6a417e
Lele97 <gabriele.grandinetti@hotmail.com>
2025-10-13 14:34:00 +0200

    fix problem eureka environment variables fix problem mail service load balancer

--- mail/src/main/java/com/pagatu/mail/config/WebClientConfig.java
+++ mail/src/main/java/com/pagatu/mail/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.pagatu.mail.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -35,6 +36,7 @@ public class WebClientConfig {
      * @return a configured WebClient.Builder instance
      */
     @Bean
+    @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
